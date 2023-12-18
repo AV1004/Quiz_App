fe4c3909873fae5b8f4bcb8e711b1f711904f07b
Valay Andhariya <andhariyavalay4@gmail.com>
2023-12-18 23:41:41 +0530

    Adding The Interactive The Answer Right or Wrong Background Effect

---
+++ src/components/Answers.jsx
@@ -0,0 +1,43 @@
+import React, { useRef } from "react";
+
+export default function Answers({
+  answers,
+  seletedAnswers,
+  answerState,
+  onSelect,
+}) {
+  const Options = useRef();
+
+  if (!Options.current) {
+    Options.current = [...answers];
+    Options.current.sort(() => Math.random() - 0.5);
+  }
+
+  return (
+    <ul id="answers">
+      {Options.current.map((ans) => {
+        const isSelected = seletedAnswers === ans;
+        let cssClass = "";
+
+        if (answerState === "answered" && isSelected) {
+          cssClass = "selected";
+        }
+
+        if (
+          (answerState === "correct" || answerState === "wrong") &&
+          isSelected
+        ) {
+          cssClass = answerState;
+        }
+
+        return (
+          <li key={ans} className="answer">
+            <button onClick={() => onSelect(ans)} className={cssClass}>
+              {ans}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,74 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import QUESTIONS from "../questions";
+import TrophyIcon from "../assets/quiz-complete.png";
+import Question from "./Question";
 
 export default function Quiz() {
-  const [activeQuesionIndex, setActiveQuesionIndex] = useState(0);
+  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently Active Questions</p>;
+  const activeQurstionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
+  const quizIsComplete = activeQurstionIndex === QUESTIONS.length;
+
+  const handleSelectAns = useCallback(
+    function handleSelectAns(selectedAnswer) {
+      setAnswerState("answered");
+      setUserAnswers((prevAnswers) => {
+        return [...prevAnswers, selectedAnswer];
+      });
+
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQurstionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [activeQurstionIndex]
+  );
+
+  const handleSkipAnswer = useCallback(() => {
+    handleSelectAns(null);
+  }, [handleSelectAns]);
+
+  // const shffleAnswers = (answers) => {
+  //   while (Options.length < 4) {
+  //     length = QUESTIONS[activeQurstionIndex].answers.length;
+  //     let ShffledChoiceIndex = Math.floor(Math.random() * length);
+  //     if (Options.includes(answers[ShffledChoiceIndex])) {
+  //       continue;
+  //     } else {
+  //       Options.push(answers[ShffledChoiceIndex]);
+  //     }
+  //   }
+  // };
+
+  if (quizIsComplete === true) {
+    return (
+      <div id="summary">
+        <img src={TrophyIcon} alt="Trophy Icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQurstionIndex}
+        questionText={QUESTIONS[activeQurstionIndex].text}
+        answers={QUESTIONS[activeQurstionIndex].answers}
+        answerState={answerState}
+        selectedAnswer={userAnswers[userAnswers.length - 1]}
+        onSelectAnswer={handleSelectAns}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
 }
